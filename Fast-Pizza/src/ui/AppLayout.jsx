676cc712cc7295fc68feeb209efbb7dc92bb7a28
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-03 15:14:40 +0530

    Lotta of things

---
+++ Fast-Pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,19 @@
+import Cart from "../features/cart/Cart";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
