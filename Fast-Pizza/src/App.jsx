cbe0055130441c61452f1552d978344fedf6d48b
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-03 16:01:59 +0530

    Fetching order details from params

--- Fast-Pizza/src/App.jsx
+++ Fast-Pizza/src/App.jsx
@@ -4,7 +4,7 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -17,12 +17,16 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         errorElement: <Error />,
-
         loader: menuLoader,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        errorElement: <Error />,
+        loader: orderLoader,
+      },
     ],
   },
 ]);
