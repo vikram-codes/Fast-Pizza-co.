4636569677ee6ea5f99c427a69effae2d47e1dd0
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-04 13:44:57 +0530

    deriving the state from useNavigation

--- Fast-Pizza/src/features/order/CreateOrder.jsx
+++ Fast-Pizza/src/features/order/CreateOrder.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -36,6 +36,9 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "isSubmitting";
 
   return (
     <div>
@@ -74,7 +77,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "...placing order" : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
