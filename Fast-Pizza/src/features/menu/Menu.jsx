d76949ad3020d9d4f1a1f9910401c97b0c29f47b
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-05 17:22:03 +0530

    Styling through tailwindcss

--- Fast-Pizza/src/features/menu/Menu.jsx
+++ Fast-Pizza/src/features/menu/Menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
   return (
     <h1>
       Menu
-      <ul>
+      <ul className="divide-y divide-stone-200 px-2">
         {menu.map((pizza) => (
           <MenuItem pizza={pizza} key={pizza.id} />
         ))}
