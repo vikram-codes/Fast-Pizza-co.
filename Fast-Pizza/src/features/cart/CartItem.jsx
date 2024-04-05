3eecfa0b9c66ec3839c88f7915558f61dd4bd694
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-05 18:27:02 +0530

    Styling through tailwindcss

--- Fast-Pizza/src/features/cart/CartItem.jsx
+++ Fast-Pizza/src/features/cart/CartItem.jsx
@@ -5,11 +5,11 @@ function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="py-3">
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between sm:gap-6">
         <p>{formatCurrency(totalPrice)}</p>
         <Button type="small">delete</Button>
       </div>
