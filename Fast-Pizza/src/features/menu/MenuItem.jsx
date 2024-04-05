725277d968806af7532984b288c2d5ef69ebc8c9
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-05 17:30:59 +0530

    Styling through tailwindcss

--- Fast-Pizza/src/features/menu/MenuItem.jsx
+++ Fast-Pizza/src/features/menu/MenuItem.jsx
@@ -1,3 +1,4 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
@@ -5,12 +6,14 @@ function MenuItem({ pizza }) {
 
   return (
     <li className="flex gap-4 py-2">
-      <img src={imageUrl} alt={name} className="h-24"/>
-      <div className="flex flex-col">
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div className="mt-auto">
+      <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-60 grayscale' : ''}`}/>
+      <div className="flex flex-col grow">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">{ingredients.join(", ")}</p>
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className='text-sm font-medium uppercase text-stone-500'>Sold out</p>}
+
+          <Button type='primary'>Add to cart</Button>
         </div>
       </div>
     </li>
