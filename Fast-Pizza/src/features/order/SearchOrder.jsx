9dfd1e3b8076de1e3a4e2a436610e03e75a1707e
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-05 13:32:36 +0530

    Styling through tailwindcss

--- Fast-Pizza/src/features/order/SearchOrder.jsx
+++ Fast-Pizza/src/features/order/SearchOrder.jsx
@@ -14,7 +14,7 @@ function SearchOrder() {
   return (
     <form onSubmit={handleSubmit} className="">
       <input
-        className="text-center"
+        className="rounded-full px-4 py-2 text-sm bg-yellow-200 placeholder:text-stone-400 transition-all duration-400 focus:outline-none w-28 sm:focus:w-72 focus:ring-yellow-500 sm:w-64 focus:ring-opacity-50"
         value={query}
         placeholder="Search your order #"
         onChange={(e) => setQuery(e.target.value)}
