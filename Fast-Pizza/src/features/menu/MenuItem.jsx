940dc2a78f4da44acf81d0a99ba125ffd9f48aef
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-07 18:49:00 +0530

    Adding items to cart

--- Fast-Pizza/src/features/menu/MenuItem.jsx
+++ Fast-Pizza/src/features/menu/MenuItem.jsx
@@ -1,23 +1,54 @@
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
-      <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-60 grayscale' : ''}`}/>
-      <div className="flex flex-col grow pt-0.5">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-60 grayscale" : ""}`}
+      />
+      <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
-        <p className="text-sm capitalize italic text-stone-500">{ingredients.join(", ")}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
         <div className="mt-auto flex items-center justify-between">
-          {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className='text-sm font-medium uppercase text-stone-500'>Sold out</p>}
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
 
-          <Button type='small'>Add to cart</Button>
+          {!soldOut ? (
+            <Button onClick={handleAddToCart} type="small">
+              Add to cart
+            </Button>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </li>
   );
 }
-
 export default MenuItem;
